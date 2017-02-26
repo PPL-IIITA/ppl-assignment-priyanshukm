070b5a82e6272354cb1f58b256a7877c4a92cf4e
Priyanshu Kumar <priyanshukm@gmail.com>
2017-02-27 02:52:21 +0530

    Add files via upload

---
+++ html/search/variables_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['lrating',['Lrating',['../class_gift.html#a787ad8fce0ef81e2923e5638e5ca7332',1,'Gift::Lrating()'],['../class_test_generator.html#aeac23e0badc0acc414b97a1c4dd002df',1,'TestGenerator::Lrating()']]]
+];
---
+++ html/search/variables_a.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['status',['status',['../class_boy.html#ad821b3784db5bb5e0fd7901a65862a32',1,'Boy::status()'],['../class_girl.html#a8bc99d3c68fe3bbad96c0258b04ec107',1,'Girl::status()']]]
+];
